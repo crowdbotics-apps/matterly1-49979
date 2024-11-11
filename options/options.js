be8a7316b47bcdf41e1ef584776d6139b8c6272a
Crowdbotics <team@crowdbotics.com>
2024-11-11 19:19:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "matterly1-49979",
+  url: "https://matterly1-49979.botics.co",
+  api: "https://matterly1-49979.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
